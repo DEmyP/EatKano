d6e8fdf743dad23418d72bd9cc31af2556be7228
DEmyP <101858009+DEmyP@users.noreply.github.com>
2022-03-18 14:59:19 +0800

    Update index.js

--- static/index.js
+++ static/index.js
@@ -465,11 +465,11 @@ const MODE_NORMAL = 1, MODE_ENDLESS = 2, MODE_PRACTICE = 3;
             SubmitResults();
         }
 
-        if (cps <= 5) return '试着好好练一下？';
-        if (cps <= 8) return 'TCL';
-        if (cps <= 10)  return 'TQL';
-        if (cps <= 15) return '您';
-        return '人？';
+        if (cps <= 5) return '基本阳痿了您？';
+        if (cps <= 8) return '奖励你22指';
+        if (cps <= 10)  return '送你一架歼-22';
+        if (cps <= 15) return '您怕是苟等人';
+        return '额？';
     }
 
     function toStr(obj) {
